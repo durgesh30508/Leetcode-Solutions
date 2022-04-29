33518e94390a2e1e7c4430f5e68556929ea37595
Durgesh Kumar <45592118+durgesh30508@users.noreply.github.com>
2022-04-29 13:24:48 +0530

    Create next_greater.py

---
+++ Array/next_greater.py
@@ -0,0 +1,13 @@
+d = {}
+        st = []
+        ans = []
+        
+        for x in nums:
+            while len(st) and st[-1] < x:
+                d[st.pop()] = x
+            st.append(x)
+
+        for x in findNums:
+            ans.append(d.get(x, -1))
+            
+        return ans
